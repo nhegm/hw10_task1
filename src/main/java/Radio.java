dbac3c671dd80fa33d330ba798ac0845a8ea4f0c
nhegmus@gmail.com <nhegm>
2023-05-15 17:44:59 +0800

    tests passed with mvn and jacoco

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,2 +1,70 @@
-package PACKAGE_NAME;public class Radio {
+public class Radio {
+    private int currentRadioStationNumber;
+    private int currentRadioVolume;
+
+    public int getCurrentRadioStationNumber() {
+
+        return currentRadioStationNumber;
+    }
+
+    public int getCurrentRadioVolume() {
+
+        return currentRadioVolume;
+    }
+
+    public void setCurrentRadioStationNumber(int newCurrentRadioStationNumber) {
+        if (newCurrentRadioStationNumber > 9) {
+            return;
+        }
+        if (newCurrentRadioStationNumber < 0) {
+            return;
+        }
+        currentRadioStationNumber = newCurrentRadioStationNumber;
+    }
+
+    public void nextRadioStation() {
+        getCurrentRadioStationNumber();
+        if (currentRadioStationNumber == 9) {
+            currentRadioStationNumber = 0;
+        } else {
+            currentRadioStationNumber++;
+        }
+    }
+
+    public void prevRadioStation() {
+        getCurrentRadioStationNumber();
+        if (currentRadioStationNumber == 0) {
+            currentRadioStationNumber = 9;
+        } else {
+            currentRadioStationNumber--;
+        }
+    }
+
+    public void incRadioVolumeBy1() {
+        getCurrentRadioVolume();
+        if (currentRadioVolume == 100) {
+            return;
+        } else {
+            currentRadioVolume++;
+        }
+    }
+
+    public void decRadioVolumeBy1() {
+        getCurrentRadioVolume();
+        if (currentRadioVolume == 0) {
+            return;
+        } else {
+            currentRadioVolume--;
+        }
+    }
+
+    public void setCurrentRadioVolume(int newCurrentRadioVolume) {
+        if (newCurrentRadioVolume > 100) {
+            return;
+        }
+        if (newCurrentRadioVolume < 0) {
+            return;
+        }
+        currentRadioVolume = newCurrentRadioVolume;
+    }
 }
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,2 +1,142 @@
-package PACKAGE_NAME;public class RadioTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    @Test
+    public void shouldGetRadioStation() {
+        Radio radioTesting = new Radio();
+
+        int expected = 0;
+        int actual = radioTesting.getCurrentRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetRadioStation() {
+        Radio radioTesting = new Radio();
+
+        radioTesting.setCurrentRadioStationNumber(7);
+
+        int expected = 7;
+        int actual = radioTesting.getCurrentRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetRadioStationWhenBelow() {
+        Radio radioTesting = new Radio();
+
+        radioTesting.setCurrentRadioStationNumber(-1);
+
+        int expected = 0;
+        int actual = radioTesting.getCurrentRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetRadioStationWhenAbove() {
+        Radio radioTesting = new Radio();
+
+        radioTesting.setCurrentRadioStationNumber(10);
+
+        int expected = 0;
+        int actual = radioTesting.getCurrentRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSwitchToNextRadioStation() {
+        Radio radioTesting = new Radio();
+
+        radioTesting.nextRadioStation();
+
+        int expected = 1;
+        int actual = radioTesting.getCurrentRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSwitchToNextRadioStationWhen9thStation() {
+        Radio radioTesting = new Radio();
+
+        radioTesting.setCurrentRadioStationNumber(9);
+        radioTesting.nextRadioStation();
+
+        int expected = 0;
+        int actual = radioTesting.getCurrentRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGetRadioVolume() {
+        Radio radioTesting = new Radio();
+
+        int expected = 0;
+        int actual = radioTesting.getCurrentRadioVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddVolumeWhenInRange() {
+        Radio volumeTesting = new Radio();
+
+        volumeTesting.setCurrentRadioVolume(1);
+        volumeTesting.incRadioVolumeBy1();
+
+        int expected = 2;
+        int actual = volumeTesting.getCurrentRadioVolume();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldDecreaseVolumeWhenInRange() {
+        Radio volumeTesting = new Radio();
+
+        volumeTesting.setCurrentRadioVolume(99);
+        volumeTesting.decRadioVolumeBy1();
+
+        int expected = 98;
+        int actual = volumeTesting.getCurrentRadioVolume();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldNotAddVolumeWhenWhenAbove() {
+        Radio volumeTesting = new Radio();
+
+        volumeTesting.setCurrentRadioVolume(100);
+        volumeTesting.incRadioVolumeBy1();
+
+        int expected = 100;
+        int actual = volumeTesting.getCurrentRadioVolume();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldNotDecreaseVolumeWhenWhenBelow() {
+        Radio volumeTesting = new Radio();
+
+        volumeTesting.setCurrentRadioVolume(0);
+        volumeTesting.decRadioVolumeBy1();
+
+        int expected = 0;
+        int actual = volumeTesting.getCurrentRadioVolume();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
 }
